0fb12798d32630a82f1f56462a676662f6c272c1
ramedina98 <b_stone99@outlook.es>
2023-09-29 09:48:42 -0600

    iniciamos

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html lang="es-MX"> <!-- Pusimos el lenguaje y la region, osea de que españo queremos que sea... -->
+    <head>
+        <!-- Todas estas son las meta etiquetas principales que debe de llebar una pagina web... -->
+        <meta charset="UTF-8">
+        <meta name="author" content="Ricardo Abraham Medina Martin Del Campi">
+        <meta name="description" content="Mi primera pagina web en un servidor">
+        <meta name="keywords" content="UNEDL, programación web, HTML5, css, javascript, PHP">
+        <meta name="robots" content="index, follow">
+        <meta name="<viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="cache_control" content="no-cache">
+        <!-- Aqui teniamos una etiqueta base, hay que ver como usarla y tambien poder tener imagenes... -->
+        <title>Programación web &AMP; Internet</title>
+        <link rel="icon" href="imagenes/favicon-16x16.png">
+        <!-- nota: El error que me marcaba era el 403, osea que no tenia el permiso correspondiente para 
+        accedeer a la carpeta en donde estaba la imagen por eso no se mostraba... -->
+        <link rel="stylesheet" href="miEstilo.css" type="text/css"/> <!-- aqui tengo mi hoja de estilos... -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+        
+    </head>
+    <body>
+        <header>
+            <img src="imagenes/logo_unedl.png" alt="UNEDL">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand">UNEDL</a>
+                    <a href="">Home</a>
+                    <a href="">Protocolos</a>
+                    <a href="">Contacto</a>
+                </div>
+            </nav>
+        </header>
+                
+        <main>
+            <section>
+                <p>Hola a todos, como va todo por alla!</p>
+            </section>
+            
+            <aside>
+                <p>Hola que onda</p>
+            </aside>
+        </main>
+        
+        <footer>
+            <p>Aqui veras informacion institucional</p>
+        </footer>
+        
+        <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
